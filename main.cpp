9e17f4dba253b1069055c10dd8d793472dda9627
Fox-nano <100073709+Fox-nano@users.noreply.github.com>
2023-02-16 21:36:13 +0800

    Add Test Files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//This is a Class Fox 
+//It's in V1.1 coming song......
+
+int main() {
+	cout<<"This is a Test"<<endl;
+	cout<<"Thank you see my program!"<<endl;
+	cout<<"The V1.1Beta is coming at 2/19 !"<<endl;
+	return 114514;
+}
